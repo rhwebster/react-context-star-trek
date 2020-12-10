0525e8d40e864d1cf5727ed01055345a433acfd1
Ryan Webster <ryan.h.webster@gmail.com>
2020-12-10 12:58:12 -0800

    defining context

---
+++ src/context/AppContext.js
@@ -0,0 +1,15 @@
+import { initialCards } from "./mockdata/CardData";
+
+const AppContext = React.createContext();
+
+const AppContextProvider = ({ children }) => {
+    return (
+        <AppContext.Provider value={{
+            cards: initialCards
+        }}>
+            {children}
+        </AppContext.Provider>
+    );
+}
+
+export default AppContext;
